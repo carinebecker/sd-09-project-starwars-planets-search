a9dd9d1dbc4fc68e619f0165aff0756874d1b257
saulokirchmaier <saulokirchmaier@gmail.com>
2021-04-20 17:44:40 -0300

    Ex. 1 - lint

--- src/service/starWarsAPI.js
+++ src/service/starWarsAPI.js
@@ -1,6 +1,5 @@
-// import data from './dataStarWars';
-
 const planetsURL = 'https://swapi-trybe.herokuapp.com/api/planets/';
+
 const getPlanets = async () => {
   const planetsFetch = await fetch(planetsURL);
   const { results } = await planetsFetch.json();
@@ -9,20 +8,5 @@ const getPlanets = async () => {
     return result;
   });
 };
-
-// const TIMEOUT = 500;
-
-// const simulateRequest = (response) => (callback) => {
-//   setTimeout(() => {
-//     callback(response);
-//   }, TIMEOUT);
-// };
-
-// export const getPlanets = () => (
-//   new Promise((resolve) => {
-//     const planets = data.results;
-//     simulateRequest(planets)(resolve);
-//   })
-// );
 
 export default getPlanets;
