ca661c012bdb61389fcd9b7564c5cfb6f3944de8
Renato Feitosa <pythaqua@gmail.com>
2021-04-20 18:27:06 -0300

    chore(App): Create basic components and import 'em

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Provider } from './context';
+import Search from './components/Search';
+import Table from './components/Table';
 import './App.css';
 
 function App() {
   return (
     <Provider>
-      <span>Hello, App!</span>
+      <Search />
+      <Table />
     </Provider>
   );
 }
