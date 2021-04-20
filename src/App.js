a17d493c33c0dd324e5250e34402c6fbf69e577e
Khyrki <khyrkimitsunobi@gmail.com>
2021-04-20 16:15:32 -0300

    Implementa funcao dentro do provider que faz a requisicao da api e atribui o resultado ao stado global

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
 import Table from './components/Table';
+import TableProvider from './context/TableProvider';
 
 function App() {
   return (
-    <Table />
+    <TableProvider>
+      <Table />
+    </TableProvider>
   );
 }
 
--- src/context/TableProvider.js
+++ src/context/TableProvider.js
@@ -1,13 +1,31 @@
-import React from 'react';
-import { Children } from 'react';
+import React, { useState } from 'react';
+import { func } from 'prop-types';
 import TableContext from './TableContext';
+import fechStarWarsAPI from '../service/StarWarsApi';
 
-function TableProvider() {
+function TableProvider({ children }) {
+  const [isLoading, setIsLoading] = useState(false);
+  const [planets, setPlanets] = useState([]);
+
+  const getPlanets = async () => {
+    // isLoading true
+    // fazer a requisição para buscar as perguntas
+    // isLoading false
+    setIsLoading(true);
+    const planetsApiResult = await fechStarWarsAPI();
+    setPlanets(planetsApiResult);
+    setIsLoading(false);
+  };
+
   return (
-    <TableContext.Provider value={}>
-      {Children}
+    <TableContext.Provider value={ { isLoading, planets, getPlanets } }>
+      {children}
     </TableContext.Provider>
   );
 }
+
+TableProvider.propTypes = {
+  children: func,
+}.isRequired;
 
 export default TableProvider;
