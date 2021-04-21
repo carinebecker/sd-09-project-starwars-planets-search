9629565ab52c67ed9c36d54b97a6ab99ac0de6e7
Kamilavrhcp <kamilavrhcp@hotmail.com>
2021-04-21 18:39:16 -0300

    Feat: lint

--- src/context/StarWarsProvider.js
+++ src/context/StarWarsProvider.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 import StarWarsContext from './StarWarsContext';
 import getPlanets from '../service/StarWarsApi';
 
@@ -22,4 +23,7 @@ function StarWarsProvider({ children }) {
   );
 }
 
+StarWarsProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
 export default StarWarsProvider;
--- src/service/StarWarsApi.js
+++ src/service/StarWarsApi.js
@@ -2,9 +2,7 @@ async function getPlanets() {
   try {
     const response = await fetch('https://swapi-trybe.herokuapp.com/api/planets/');
     const data = await response.json();
-    data.results.map((planet) => {
-      delete planet.residents;
-    });
+    data.results.map((planet) => delete planet.residents);
     return data.results;
     // remover a chave residents
   } catch (error) {
