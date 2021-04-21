1a4187b5c6a53a3ce93f5b4045a5f5f34c9c59c2
Tiago de Almeida Paz <tiagopaz.dev@gmail.com>
2021-04-21 12:20:11 -0300

    req 02

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,38 +1,89 @@
-import React, { useEffect, useContext } from 'react';
+import React, { useEffect, useContext, useState } from 'react';
 import MyContext from '../context/MyContext';
 import requestAPI from '../service/request';
 
 function Table() {
-  const { data, setPlanets } = useContext(MyContext);
+  const {
+    data, setData, filters, setFilters, loading, setLoading } = useContext(MyContext);
+  const { filterByName } = filters.filters;
+  const [dataFilter, setDataFilter] = useState([]);
+
+  function handleChange({ target }) {
+    setFilters({
+      filters: {
+        filterByName: {
+          name: target.value,
+        },
+      },
+    });
+    const filter = data.filter((r) => (
+      r.name.includes(target.value)
+    ));
+    console.log(filter);
+    setDataFilter(filter);
+  }
+
   function renderTable() {
     return (
-      <table>
-        <thead>
-          <tr>
-            { Object.keys(data[0]).map((r) => <th key={ r }>{ r }</th>) }
-          </tr>
-        </thead>
-        <tbody>
-          { data.map((r) => (
-            <tr key={ r.name }>
-              { Object.values(r).map((obj) => <td key={ obj }>{ obj }</td>) }
-            </tr>))}
-        </tbody>
-      </table>
+      <div>
+        <label htmlFor="filter">
+          Filtrar
+          <input
+            type="text"
+            id="filter"
+            value={ filterByName.name }
+            onChange={ handleChange }
+            data-testid="name-filter"
+          />
+        </label>
+        <table>
+          <thead>
+            <tr>
+              { Object.keys(dataFilter[0]).map((r) => <th key={ r }>{ r }</th>) }
+            </tr>
+          </thead>
+          <tbody>
+            { dataFilter.map((r) => (
+              <tr key={ r.name }>
+                { Object.values(r).map((obj) => <td key={ obj }>{ obj }</td>) }
+              </tr>))}
+          </tbody>
+        </table>
+      </div>
     );
   }
 
   useEffect(() => {
-    requestAPI().then((r) => setPlanets(r));
-  }, [setPlanets]);
+    requestAPI().then((r) => {
+      setDataFilter(r);
+      return setData(r);
+    }).then(() => setLoading(false));
+  }, [setData, setLoading]);
 
-  if (data.length !== 0) {
+  if (loading === false && dataFilter.length > 0) {
     return (
       renderTable()
     );
   }
+  if (dataFilter.length === 0) {
+    return (
+      <div>
+        <label htmlFor="filter">
+          Filtrar
+          <input
+            type="text"
+            id="filter"
+            value={ filterByName.name }
+            onChange={ handleChange }
+            data-testid="name-filter"
+          />
+        </label>
+        <h4>Ops, parece que não há planetas com esse nome</h4>
+      </div>
+    );
+  }
   return (
-    <h2>teste</h2>
+    <h2>Loading...</h2>
   );
 }
 
--- src/context/MyProvider.js
+++ src/context/MyProvider.js
@@ -3,11 +3,21 @@ import PropTypes from 'prop-types';
 import MyContext from './MyContext';
 
 function MyProvider({ children }) {
-  const [data, setPlanets] = useState([]);
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [filters, setFilters] = useState({
+    filters: {
+      filterByName: {
+        name: '',
+      },
+    },
+  });
 
   return (
     <main>
-      <MyContext.Provider value={ { data, setPlanets } }>
+      <MyContext.Provider
+        value={ { data, setData, filters, setFilters, loading, setLoading } }
+      >
         { children }
       </MyContext.Provider>
     </main>
