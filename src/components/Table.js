86184a502558e299288987bb8673f906b66371dc
Pedro Henrique <pedroenrique.400@gmail.com>
2021-04-20 22:49:56 -0300

    faz requisito 2/eslint/3

--- src/components/Table.js
+++ src/components/Table.js
@@ -2,7 +2,12 @@ import React, { useContext } from 'react';
 import { StarWarsContext } from '../context/index';
 
 const Table = () => {
-  const { allPlanets, loading, planetsWithFilter, handleChange } = useContext(StarWarsContext);
+  const {
+    allPlanets,
+    loading,
+    planetsWithFilter,
+    handleChange,
+  } = useContext(StarWarsContext);
 
   const renderTable = (planets) => (
     <table>
@@ -21,25 +26,24 @@ const Table = () => {
           <th>Criado</th>
           <th>Editado</th>
           <th>Url</th>
-
         </tr>
       </thead>
       <tbody>
         {planets.map((planet) => (
-          <tr key={planet.name}>
-            <td>{planet.name}</td>
-            <td>{planet.rotation_period}</td>
-            <td>{planet.orbital_period}</td>
-            <td>{planet.diameter}</td>
-            <td>{planet.climate}</td>
-            <td>{planet.gravity}</td>
-            <td>{planet.terrain}</td>
-            <td>{planet.surface_water}</td>
-            <td>{planet.population}</td>
-            <td>{planet.films}</td>
-            <td>{planet.created}</td>
-            <td>{planet.edited}</td>
-            <td>{planet.url}</td>
+          <tr key={ planet.name }>
+            <td>{ planet.name }</td>
+            <td>{ planet.rotation_period }</td>
+            <td>{ planet.orbital_period }</td>
+            <td>{ planet.diameter }</td>
+            <td>{ planet.climate }</td>
+            <td>{ planet.gravity }</td>
+            <td>{ planet.terrain }</td>
+            <td>{ planet.surface_water }</td>
+            <td>{ planet.population }</td>
+            <td>{ planet.films }</td>
+            <td>{ planet.created }</td>
+            <td>{ planet.edited }</td>
+            <td>{ planet.url }</td>
           </tr>
         ))}
       </tbody>
