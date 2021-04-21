c0191e4617a1ce0688bab46f913bceb6313c3cce
J.Henrique <jhenriqueribeirodesa@gmail.com>
2021-04-21 17:27:06 -0300

    Add table of planets

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import Table from './components/Table';
 
 function App() {
+  const [planets, setPlanets] = useState([]);
+  useEffect(() => {
+    const apiUrl = 'https://swapi-trybe.herokuapp.com/api/planets/';
+    fetch(apiUrl).then((res) => res.json()).then(({ results }) => setPlanets(results));
+  }, []);
+
   return (
-    <span>Hello, App!</span>
+    <Table planets={ planets } />
   );
 }
 
