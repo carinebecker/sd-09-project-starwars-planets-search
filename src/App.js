ad5c1fb2a4ee53683280c68f4d00de6d502869c0
J.Henrique <jhenriqueribeirodesa@gmail.com>
2021-04-21 19:15:50 -0300

    Add filter by name to planets

--- src/App.js
+++ src/App.js
@@ -4,13 +4,26 @@ import Table from './components/Table';
 
 function App() {
   const [planets, setPlanets] = useState([]);
+  const [filteredPlanets, setFilteredPlanets] = useState([]);
+  const [name, setName] = useState('');
   useEffect(() => {
     const apiUrl = 'https://swapi-trybe.herokuapp.com/api/planets/';
     fetch(apiUrl).then((res) => res.json()).then(({ results }) => setPlanets(results));
   }, []);
 
+  useEffect(() => {
+    setFilteredPlanets(planets.filter((planet) => planet.name.includes(name)));
+  }, [name, planets]);
+
   return (
-    <Table planets={ planets } />
+    <div>
+      <input
+        data-testid="name-filter"
+        value={ name }
+        onChange={ ({ target }) => setName(target.value) }
+      />
+      <Table planets={ filteredPlanets } />
+    </div>
   );
 }
 
