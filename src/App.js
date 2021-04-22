94a4bc26d8c4344c3f87b9c26ad64280b36f521b
Jose Daniel Arreaza <jdaniell.93@gmail.com>
2021-04-22 16:08:13 -0300

    fixing bonus

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import './App.css';
+import StateProvider from './context/stateProvider';
+import Table from './components/Table';
+import RemoveFilter from './components/RemoveFilter';
+import AddFilters from './components/AddFilters';
 
 function App() {
   return (
-    <span>Hello, App!</span>
+    <StateProvider>
+      <AddFilters />
+      <RemoveFilter />
+      <Table />
+    </StateProvider>
   );
 }
 
