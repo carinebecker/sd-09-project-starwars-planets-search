4e003246cd1976c13e5f73c12ae39fba85fbded2
Rafael Pufal <rafael.pufal23@gmail.com>
2021-04-22 13:16:44 -0300

    req 3

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,13 +1,22 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import PlanetContext from '../context/PlanetContext';
 
 function Filters() {
-  const { filters, setFilters } = useContext(PlanetContext);
+  const { filters, setFilters, setNumericValuesFilter } = useContext(PlanetContext);
+  const [columnFilters, setColumnFilters] = useState({
+    column: '', value: '', comparison: '',
+  });
 
   const getSearchInput = ({ target }) => {
     const { value } = target;
     const filterObj = { ...filters, filterByName: { name: value } };
     setFilters(filterObj);
+  };
+
+  const numericValuesFilter = ({ target: { name, value } }) => {
+    if (name === 'column') setColumnFilters({ ...columnFilters, column: value });
+    if (name === 'comparison') setColumnFilters({ ...columnFilters, comparison: value });
+    if (name === 'value') setColumnFilters({ ...columnFilters, value });
   };
 
   const searchInput = () => (
@@ -21,9 +30,63 @@ function Filters() {
       />
     </label>
   );
+  const columnFilter = () => (
+    <select
+      data-testid="column-filter"
+      name="column"
+      onChange={ numericValuesFilter }
+    >
+      <option value="">Select</option>
+      <option value="population">population</option>
+      <option value="orbital_period">orbital_period</option>
+      <option value="diameter">diameter</option>
+      <option value="rotation_period">rotation_period</option>
+      <option value="surface_water">surface_water</option>
+    </select>
+  );
+  const comparisonFilter = () => (
+    <select
+      data-testid="comparison-filter"
+      name="comparison"
+      onChange={ numericValuesFilter }
+    >
+      <option value="">Select</option>
+      <option value="maior que">maior que</option>
+      <option value="menor que">menor que</option>
+      <option value="igual a">igual a</option>
+    </select>
+  );
+  const valueFilter = () => (
+    <input
+      type="number"
+      onChange={ numericValuesFilter }
+      name="value"
+      placeholder="valor"
+      data-testid="value-filter"
+    />
+  );
+  const handleClick = () => {
+    setFilters(
+      { ...filters,
+        filterByNumericValues: [{ ...columnFilters }] },
+    );
+    setNumericValuesFilter(true);
+  };
 
   return (
-    searchInput()
+    <form>
+      {searchInput()}
+      {columnFilter()}
+      {comparisonFilter()}
+      {valueFilter()}
+      <button
+        type="button"
+        data-testid="button-filter"
+        onClick={ handleClick }
+      >
+        Filtrar
+      </button>
+    </form>
   );
 }
 
--- src/context/PlanetProvider.js
+++ src/context/PlanetProvider.js
@@ -4,13 +4,19 @@ import PlanetContext from './PlanetContext';
 import getPlanets from '../services/starwarsApi';
 
 function PlanetProvider({ children }) {
-  const baseFilter = { filterByName:
-      { name: '' },
+  const baseFilter = {
+    filterByName: {
+      name: '',
+    },
+    filterByNumericValues: [{
+      column: '', value: '', comparison: '',
+    }],
   };
   const [data, setData] = useState([]);
   const [dataToFilter, setDataToFilter] = useState([]);
   const [loading, setLoading] = useState(true);
   const [filters, setFilters] = useState(baseFilter);
+  const [numericValuesFilterOn, setNumericValuesFilter] = useState(false);
 
   const context = {
     dataToFilter,
@@ -18,6 +24,7 @@ function PlanetProvider({ children }) {
     loading,
     filters,
     setFilters,
+    setNumericValuesFilter,
   };
 
   const getData = async () => {
@@ -36,6 +43,30 @@ function PlanetProvider({ children }) {
     setDataToFilter(planetsToBeFiltered);
   },
   [data, filters]);
+
+  useEffect(() => {
+    if (numericValuesFilterOn) {
+      const { filterByNumericValues: [{ column, comparison, value }] } = filters;
+      let planetsToBeFiltered = data;
+      const valueNumber = parseInt(value, 10);
+      if (comparison === 'maior que') {
+        planetsToBeFiltered = planetsToBeFiltered.filter(
+          (planet) => planet[column] > valueNumber,
+        );
+      }
+      if (comparison === 'menor que') {
+        planetsToBeFiltered = planetsToBeFiltered.filter(
+          (planet) => planet[column] < valueNumber,
+        );
+      }
+      if (comparison === 'igual a') {
+        planetsToBeFiltered = planetsToBeFiltered.filter(
+          (planet) => planet[column] === value,
+        );
+      }
+      setDataToFilter(planetsToBeFiltered);
+    }
+  }, [data, filters, numericValuesFilterOn]);
 
   useEffect(() => {
     getData();
