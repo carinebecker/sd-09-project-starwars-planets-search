50c06b2f32cc5aca40eeedba18e659e32bda53f1
LucioOSilva <lucioosilva00@gmail.com>
2021-04-22 08:35:05 -0300

    criado contexto para planetas filtrados

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from 'react';
 import PlanetsContext from '../context/PlanetsContext';
 
 function Table() {
-  const { data } = useContext(PlanetsContext);
+  const { filteredPlanets } = useContext(PlanetsContext);
   return (
     <table>
       <thead>
@@ -23,7 +23,7 @@ function Table() {
         </tr>
       </thead>
       <tbody>
-        {data.map((planet) => (
+        {filteredPlanets.map((planet) => (
           <tr key={ planet.name }>
             <td>{planet.name}</td>
             <td>{planet.climate}</td>
--- src/context/PlanetsProvider.js
+++ src/context/PlanetsProvider.js
@@ -3,20 +3,40 @@ import PropTypes from 'prop-types';
 import PlanetsContext from './PlanetsContext';
 import fetchPlanets from '../services/apiService';
 
+const defaultFilters = {
+  filters:
+  {
+    filterByName: {
+      name: ''
+    },
+    filterByNumericValues: [
+      {
+        column: '',
+        comparison: 'maior que',
+        value: '',
+      }
+    ]
+  }
+}
+
+
 function PlanetsProvider({ children }) {
   const [data, setData] = useState([]);
+  const [filters, setFilters] = useState(defaultFilters);
+  const [filteredPlanets, setFilteredPlanets] = useState([]);
 
   useEffect(() => {
     const getPlanets = async () => {
       const planetsList = await fetchPlanets();
       setData(planetsList);
+      setFilteredPlanets(planetsList);
     };
     if (data.length === 0) {
       getPlanets();
     }
   }, [data]);
 
-  const context = { data };
+  const context = { filteredPlanets, filters, setFilters };
   return (
     <main>
       <PlanetsContext.Provider value={ context }>
