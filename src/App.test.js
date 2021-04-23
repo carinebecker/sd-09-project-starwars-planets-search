2514b3b5eeb50297358ec75b5140818f3df8a909
ggaldino95 <ggaldino95@gmail.com>
2021-04-22 21:17:49 -0300

    [Task 3] Removing teste file comments

--- src/App.test.js
+++ src/App.test.js
@@ -286,7 +286,7 @@ describe('4 - Não utilize filtros repetidos', () => {
   });
 });
 
-/* describe('5 - Apague o filtro de valores numéricos e desfaça as filtragens dos dados da tabela ao clicar no ícone de `X` de um dos filtros', () => {
+describe('5 - Apague o filtro de valores numéricos e desfaça as filtragens dos dados da tabela ao clicar no ícone de `X` de um dos filtros', () => {
   beforeAll(mockFetch);
   beforeEach(cleanup);
 
@@ -365,4 +365,3 @@ describe('6 - Ordene as colunas de forma ascendente ou descendente', () => {
     expect(actual).toEqual(expected);
   });
 });
- */
