437a744c89b12982cd7a2cf335b7dc119d4e1087
Osvaldo Borges <netoo70@gmail.com>
2021-04-23 12:06:39 -0300

    Osvaldo became a Jedi

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css';
+import Provider from './provider/StarWarsProvider';
+import NewApp from './NewApp';
 
 function App() {
   return (
-    <span>Hello, App!</span>
+    <Provider>
+      <NewApp />
+    </Provider>
   );
 }
 
