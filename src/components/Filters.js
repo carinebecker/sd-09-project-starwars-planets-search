76ed0d847f703498e8ec127d6af0648d5a826ccf
genadireuben <genadireuben91@gmail.com>
2021-04-24 16:14:00 -0300

    requisito 5: implementa remoçao de filtros

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -2,14 +2,23 @@ import React, { useContext } from 'react';
 import PlanetsContext from '../context/PlanetsContext';
 
 export default function Filters() {
-  const { filters } = useContext(PlanetsContext);
+  const { filters, setFilters,
+    columnSelect, setColumnSelect } = useContext(PlanetsContext);
+
+  const handleClick = (column) => {
+    setColumnSelect([...columnSelect, column]);
+    const newFilter = filters.filterByNumericValues
+      .filter((obj) => obj.column !== column);
+    setFilters({ ...filters, filterByNumericValues: newFilter });
+  };
 
   return (
     filters.filterByNumericValues.map((obj, index) => (
-      <div key={ index }>
+      <div key={ index } data-testid="filter">
         <p>{obj.column}</p>
         <p>{obj.comparison}</p>
         <p>{obj.value}</p>
+        <button type="button" onClick={ () => handleClick(obj.column) }>X</button>
       </div>))
   );
 }
