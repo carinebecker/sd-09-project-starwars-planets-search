8aae4a512cc36d316d9294caf179af3840d12509
RodrigoNeoEx <draja.acc@gmail.com>
2021-04-24 11:48:38 -0300

    changed api request, created all context

--- src/services/requestApi.js
+++ src/services/requestApi.js
@@ -1,8 +1,7 @@
 async function fetchApi() {
   const endpoint = await fetch('https://swapi-trybe.herokuapp.com/api/planets/?format=json');
   const response = await endpoint.json();
-  response.results.map((api) => delete api.residents);
-  setData(response);
+  return response.results.map((api) => delete api.residents);
 }
 
 export default fetchApi;
