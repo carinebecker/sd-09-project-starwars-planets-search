866db2d991ae62cb2c7bd188b5e583ed48f213ef
diego-vini <analistamm@gmail.com>
2021-04-24 23:48:37 -0300

    Ajustando commit_2

--- src/component/DataTablePlanets.js
+++ src/component/DataTablePlanets.js
@@ -3,38 +3,36 @@ import Context from '../context/Context';
 
 function DataTablePlanets() {
   const { response } = useContext(Context);
-  const zero = 0;
-  if (response.length > zero) {
+  const value = 0;
+  if (response.length > value) {
     const planets = Object.keys(response[0]);
     const filtered = planets.filter((item) => (
       item !== 'residents'
     ));
     return (
       <table>
-        <tbody>
-          <tr>
-            {filtered.map((planetKey) => (
-              <th key={ planetKey }>{planetKey}</th>
-            ))}
-          </tr>
-          {response.map((planetItem) => (
-            <tr key={ planetItem }>
-              <td>{planetItem.name}</td>
-              <td>{planetItem.rotation_period}</td>
-              <td>{planetItem.orbital_period}</td>
-              <td>{planetItem.diameter}</td>
-              <td>{planetItem.climate}</td>
-              <td>{planetItem.gravity}</td>
-              <td>{planetItem.terrain}</td>
-              <td>{planetItem.surface_water}</td>
-              <td>{planetItem.population}</td>
-              <td>{planetItem.films}</td>
-              <td>{planetItem.created}</td>
-              <td>{planetItem.edited}</td>
-              <td>{planetItem.url}</td>
-            </tr>
+        <tr>
+          {filtered.map((planetKey) => (
+            <th key={ planetKey }>{planetKey}</th>
           ))}
-        </tbody>
+        </tr>
+        {response.map((planetItem) => (
+          <tr key={ planetItem }>
+            <td>{planetItem.name}</td>
+            <td>{planetItem.rotation_period}</td>
+            <td>{planetItem.orbital_period}</td>
+            <td>{planetItem.diameter}</td>
+            <td>{planetItem.climate}</td>
+            <td>{planetItem.gravity}</td>
+            <td>{planetItem.terrain}</td>
+            <td>{planetItem.surface_water}</td>
+            <td>{planetItem.population}</td>
+            <td>{planetItem.films}</td>
+            <td>{planetItem.created}</td>
+            <td>{planetItem.edited}</td>
+            <td>{planetItem.url}</td>
+          </tr>
+        ))}
       </table>
     );
   }
