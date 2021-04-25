4f8237872206e368f86ae364dd0c8058a7e2b015
Rafa Reis <rafaelrnascimento2006@gmail.com>
2021-04-25 20:04:49 -0300

    estilizando projeto

--- src/components/Table.js
+++ src/components/Table.js
@@ -5,7 +5,8 @@ import { filterPlanets, sortPlanets } from './Store';
 function Table() {
   const { data, filters } = useContext(Context);
   const { filterByName, filterByNumericValues, order } = filters;
-  if (!data.results) return <div>Loading...</div>;
+
+  if (!data.results) return <div className="loading">Loading...</div>;
   const planets = sortPlanets(data.results, order);
 
   return (
