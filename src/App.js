debc4ea71d5f626bf16d8e75bd6392497859ad73
Felipe Pimentel <lipe.pimentel89@gmail.com>
2021-04-25 19:58:49 -0300

    First commit. Let's go! The Last Jedi >>>>> Rogue One

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import './App.css';
 
 function App() {
   return (
-    <span>Hello, App!</span>
+    <img
+      src="https://i.gifer.com/origin/4e/4ea1da6a8d1d729d9249de26f21d0bee.gif"
+      alt="Rich Evans excited about SW"
+    />
   );
 }
 
