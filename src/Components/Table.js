072494e9e8ad29a545600a85867eea606bb8e7d9
ViniciusChagas <viniciushrcs@gmail.com>
2021-04-26 19:33:42 -0300

    Construindo a tabela com dados

--- src/Components/Table.js
+++ src/Components/Table.js
@@ -4,12 +4,33 @@ import StarWarsContext from '../Context/StarWarsContext';
 export default function Table() {
   const { data } = useContext(StarWarsContext);
   console.log(data);
-  if (!data) return <p>Loading...</p>;
+  const rowHead = () => (
+    <tr>
+      {Object.keys(data.results[0]).map((title) => <th key={ title }>{title}</th>)}
+    </tr>
+  );
+  const rowBody = () => (
+    data.results
+      .map((element) => (
+        <tr key={ element.name }>
+          { Object.values(element).map((value) => <td key={ value }>{ value }</td>) }
+        </tr>
+      )));
+
+  // Source of the rowHead and rowBody functions: https://github.com/tryber/sd-09-project-starwars-planets-search/pull/52/files /
+
+  if (!data.results) {
+    return <p>Loading...</p>;
+  }
   return (
     <div>
       <table>
-        <thead />
-        <tbody />
+        <thead>
+          { rowHead() }
+        </thead>
+        <tbody>
+          { rowBody() }
+        </tbody>
       </table>
     </div>
   );
