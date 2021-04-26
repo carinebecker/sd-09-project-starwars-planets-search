3bb4172425518084d0aa425acf577993038181e7
matheusjkl1 <matheus.unilog@gmail.com>
2021-04-26 10:40:37 -0300

    lint fix v6

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Provider from './context/Provider';
 import Table from './components/Table';
-import './App.css';
 import Filter from './components/Filter';
 
 function App() {
