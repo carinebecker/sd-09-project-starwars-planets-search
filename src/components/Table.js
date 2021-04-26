cdc6d9ed5f446a09ec314c87f0ee203365610db7
Caio Daniel Nunes Santos <caio.daniel.nunes.santos@gmail.com>
2021-04-25 22:34:30 -0300

    Device transfer

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState, useEffect } from 'react';
+import React, { useContext, useState, useEffect, useRef } from 'react';
 import PlanetsContext from '../contexts/PlanetsContext';
 import PlanetsTable from './PlanetsTable';
 
@@ -9,7 +9,26 @@ const Table = () => {
     filterByName: {
       name: '',
     },
+    filterByNumericValues: [
+      {
+        column: 'population',
+        comparison: 'maior que',
+        value: '100000',
+      },
+    ],
   });
+
+  const columnRef = useRef(null);
+  const comparisonRef = useRef(null);
+  const valueRef = useRef(null);
+
+  const columnFields = [
+    'population',
+    'orbital_period',
+    'diameter',
+    'rotation_period',
+    'surface_water',
+  ];
 
   useEffect(() => {
     if (isFetched) {
@@ -24,20 +43,71 @@ const Table = () => {
       ? (
         <>
           <form>
-            <label htmlFor="name-filter">
-              Pesquisar por nome
-              <input
-                id="name-filter"
-                value={ filters.filterByName.name }
-                onChange={ (event) => (filtersSet({
+            Pesquisar por nome
+            <input
+              id="name-filter"
+              value={ filters.filterByName.name }
+              onChange={ (event) => (filtersSet({
+                ...filters,
+                filterByName: {
+                  name: event.target.value,
+                },
+              })) }
+              data-testid="name-filter"
+            />
+            <select
+              data-testid="column-filter"
+              ref={ columnRef }
+            >
+              {
+                columnFields.map((value, index) => (
+                  !filters.filterByNumericValues.some((val) => (
+                    val.column === value
+                  ))
+                    ? (
+                      <option
+                        key={ index }
+                        data-testid={ value }
+                        value={ value }
+                      >
+                        { value }
+                      </option>
+                    ) : null
+                ))
+              }
+            </select>
+            <select
+              data-testid="comparison-filter"
+              ref={ comparisonRef }
+            >
+              <option>maior que</option>
+              <option>menor que</option>
+              <option>igual a</option>
+            </select>
+            <input
+              type="number"
+              data-testid="value-filter"
+              ref={ valueRef }
+            />
+            <button
+              type="button"
+              data-testid="button-filter"
+              onClick={ () => {
+                filtersSet({
                   ...filters,
-                  filterByName: {
-                    name: event.target.value,
-                  },
-                })) }
-                data-testid="name-filter"
-              />
-            </label>
+                  filterByNumericValues: [
+                    ...filters.filterByNumericValues,
+                    {
+                      column: columnRef.current.value,
+                      comparison: comparisonRef.current.value,
+                      value: valueRef.current.value,
+                    },
+                  ],
+                });
+              } }
+            >
+              Adicionar Filtro
+            </button>
           </form>
           <PlanetsTable planets={ filteredData } />
         </>
