b1ce40ae7223636e1b533c3c1dcb99e4c7f6233c
ViniciusChagas <viniciushrcs@gmail.com>
2021-04-26 17:43:55 -0300

    Pasta services e fetch da API

---
+++ src/services/fetchStarWarsAPI.js
@@ -0,0 +1,7 @@
+export default async function fetchPlanets() {
+  const endpoint = 'https://swapi-trybe.herokuapp.com/api/planets';
+  const response = await fetch(endpoint);
+  const { results } = await response.json();
+  results.forEach((result) => delete result.residents);
+  return results;
+}
