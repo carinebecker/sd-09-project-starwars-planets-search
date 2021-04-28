21a454adda59e24e27af3c9c2407030982b4baca
Carine Becker <caarine.m@gmail.com>
2021-04-28 10:52:02 -0300

    feat: req1

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -3,37 +3,40 @@ import TableContext from '../context/TableContext';
 
 function Table() {
   const { data } = useContext(TableContext);
-  console.log(data);
+  const { results } = data;
+  const resultsKeys = results.map((res) => Object.keys(res));
+  const resultsValues = results.map((res) => Object.values(res));
 
-  const results = data.results.map((res) => Object.keys(res));
-  const values = data.results.map((res) => Object.values(res));
+  function renderHeader() {
+    return resultsKeys[0].map((each, i) => (
+      each === 'residents'
+        ? ''
+        : <th key={ i }>{each.toUpperCase().replace('_', ' ')}</th>
+    ));
+  }
 
-  // console.log(`results: ${results}`);
-  // const planet = results.map(each => each)
-  // console.log(planet)
+  function renderBody() {
+    const residentsIndex = 9;
+    return resultsValues.map((value, i) => (
+      <tr key={ i }>
+        {
+          value.map((currValue, index) => (
+            index !== residentsIndex ? <td key={ index }>{currValue}</td> : ''
+          ))
+        }
+      </tr>
+    ));
+  }
+
   return (
     <table>
       <thead>
         <tr>
-          {
-            results[0].map((each, i) => (
-              <th key={ i }>{each}</th>
-            ))
-          }
+          { renderHeader() }
         </tr>
       </thead>
       <tbody>
-        {
-          values.map((value, i) => (
-            <tr key={ i }>
-              {
-                value.map((currValue, index) => (
-                  <td key={ index }>{currValue}</td>
-                ))
-              }
-            </tr>
-          ))
-        }
+        { renderBody() }
       </tbody>
     </table>
   );
