695110a4711a49b8d97350c9a46f696faa46bb08
alailsonko <aalailson3@gmail.com>
2021-04-28 16:11:52 -0300

    set app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css';
+import Table from './Components/Table';
+import Provider from './services/provider';
 
 function App() {
   return (
-    <span>Hello, App!</span>
+    <Provider>
+      <Table />
+    </Provider>
   );
 }
 
