edacdca1c4aed3b1a5eb7b82aef1ecfad46f8e6f
Carine Becker <caarine.m@gmail.com>
2021-04-28 11:36:32 -0300

    feat: search bar

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Table from './components/Table';
 import './App.css';
 import TableProvider from './context/TableProvider';
+import SearchBar from './components/SearchBar';
 
 function App() {
   return (
     <div>
       <span>Planets search!</span>
+      <SearchBar />
       {/* OBRIGADA LUCIANO LODI PELA AJUDA 🙏🏻 */}
       <TableProvider>
         <Table />
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function SearchBar() {
+  return (
+    <input type="text" data-testid="name-filter" placeholder="Pesquisar planeta" />
+  );
+}
+
+export default SearchBar;
