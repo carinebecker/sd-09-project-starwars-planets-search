8e1e98b624f71dc2fdbf03dbf07c70e6caabe78d
TiagoEmanuel8 <tiago.emanuel.n@gmail.com>
2021-04-28 22:33:48 -0300

    requisito 1 - concluido

---
+++ src/components/Table.js
@@ -0,0 +1,48 @@
+import React, { useContext } from 'react';
+import PlanetsContext from '../context/PlanetsContext';
+
+function Table() {
+  const { planets } = useContext(PlanetsContext);
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Rotation_period</th>
+          <th>Orbital_period</th>
+          <th>Diameter</th>
+          <th>Climate</th>
+          <th>Gravity</th>
+          <th>Terrain</th>
+          <th>Surface_water</th>
+          <th>Population</th>
+          <th>Films</th>
+          <th>Created</th>
+          <th>Edited</th>
+          <th>URL</th>
+        </tr>
+      </thead>
+      <tbody>
+        {planets.map((element, index) => (
+          <tr key={ index }>
+            <td>{element.name}</td>
+            <td>{element.rotation_period}</td>
+            <td>{element.orbital_period}</td>
+            <td>{element.diameter}</td>
+            <td>{element.climate}</td>
+            <td>{element.gravity}</td>
+            <td>{element.terrain}</td>
+            <td>{element.surface_water}</td>
+            <td>{element.population}</td>
+            <td>{element.films}</td>
+            <td>{element.created}</td>
+            <td>{element.edited}</td>
+            <td>{element.url}</td>
+          </tr>))}
+      </tbody>
+    </table>
+  );
+}
+
+export default Table;
---
+++ src/services/api.js
@@ -0,0 +1,8 @@
+const url = 'https://swapi-trybe.herokuapp.com/api/planets/';
+
+const getPlanets = async () => {
+  const results = await (await fetch(url)).json();
+  return results;
+};
+
+export default getPlanets;
