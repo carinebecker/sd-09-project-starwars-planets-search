993217d750084f372d817535365cff6526a1ea46
BBrolesi <brunohsbrolesi@gmail.com>
2021-05-01 18:04:07 -0300

    feat: render filter component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './App.css';
+import Filter from './components/Filter';
 import PlanetsProvider from './context/Planets';
 import Planets from './pages/Planets';
 
 function App() {
   return (
-    <PlanetsProvider>
-      <Planets />
-    </PlanetsProvider>
+    <div>
+      <PlanetsProvider>
+        <Filter />
+        <Planets />
+      </PlanetsProvider>
+    </div>
   );
 }
 
