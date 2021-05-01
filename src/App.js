9740ab1f68b9c604afe1109b72720fdccc55f23f
BBrolesi <brunohsbrolesi@gmail.com>
2021-05-01 16:34:52 -0300

    feat: render planets page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css';
+import PlanetsProvider from './context/Planets';
+import Planets from './pages/Planets';
 
 function App() {
   return (
-    <span>Hello, App!</span>
+    <PlanetsProvider>
+      <Planets />
+    </PlanetsProvider>
   );
 }
 
