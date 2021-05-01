ab9ddca9c13249034fd27d0cf0ccf69437e49658
BBrolesi <brunohsbrolesi@gmail.com>
2021-05-01 16:34:23 -0300

    feat: create planets page

---
+++ src/pages/Planets.jsx
@@ -0,0 +1,18 @@
+import React, { useEffect } from 'react';
+import Table from '../components/Table';
+import { usePlanets } from '../context/Planets';
+import fetchPlanets from '../services';
+
+const Planets = () => {
+  const { setData } = usePlanets();
+  useEffect(() => {
+    fetchPlanets().then((planets) => setData(planets));
+  }, []);
+  return (
+    <div className="planets-container">
+      <Table />
+    </div>
+  );
+};
+
+export default Planets;
