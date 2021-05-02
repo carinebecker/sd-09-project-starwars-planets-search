ee54985ed1fe026bbd2f0f739373d063873baaa6
vinicius <viniciuspalenske@gmail.com>
2021-05-02 15:05:57 -0300

    [fix] fix

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import { Header, Table } from './components';
 import PlanetsProvider from './context/PlanetsProvider';
-import './css/App.css';
 
 function App() {
   return (
     <PlanetsProvider>
-      <div className="align-text">
-        <Header />
-        <Table />
-      </div>
+      <Header />
+      <Table />
     </PlanetsProvider>
   );
 }
