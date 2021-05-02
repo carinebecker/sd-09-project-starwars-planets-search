ba2559131c7945256d8f43f81c9f8cc0d9f19d57
vinicius <viniciuspalenske@gmail.com>
2021-05-02 14:52:24 -0300

    [fix] Mudando o Provider do index.js para app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import { Header, Table } from './Components/index';
-import './CSS/index';
+import { Header, Table } from './Components';
+import PlanetsProvider from './Context/PlanetsProvider';
+import './CSS';
 
 function App() {
   return (
-    <div className="align-text">
-      <Header />
-      <Table />
-    </div>
+    <PlanetsProvider>
+      <div className="align-text">
+        <Header />
+        <Table />
+      </div>
+    </PlanetsProvider>
   );
 }
 
