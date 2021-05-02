3eee8284a44933015b8fc0b7f2c6acc0c24546c6
vinicius <viniciuspalenske@gmail.com>
2021-05-02 14:23:09 -0300

    [feat]Criação da Tabela

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import './Pages/CSS/index';
-import { Header } from './Pages/index';
+import { Header, Table } from './Components/index';
+import './CSS/index';
 
 function App() {
   return (
     <div className="align-text">
       <Header />
+      <Table />
     </div>
   );
 }
--- src/Pages/CSS/index.js
+++
@@ -1,2 +0,0 @@
-export { default as AppCss } from './App.css';
-export { default as HeaderCss } from './Header.css';
