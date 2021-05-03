319e1ac3bfa49698352efbf86c60576a34787b8b
Carine Becker <caarine.m@gmail.com>
2021-05-02 23:49:10 -0300

    fix: req3

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -30,15 +30,16 @@ function Filters() {
     });
   };
 
+  //VERIFICAR OS TESTES DO REQUISITO 3 ! Boa noite!
+
   const handleClick = () => {
     const { column, comparison, value } = filterByNumeric;
-    console.log(filterByNumeric);
     let result;
     if (comparison === 'greater') {
-      result = results.filter((currPlanet) => currPlanet[column] >= Number(value));
+      result = results.filter((currPlanet) => currPlanet[column] > Number(value));
     }
     if (comparison === 'less') {
-      result = results.filter((currPlanet) => currPlanet[column] <= Number(value));
+      result = results.filter((currPlanet) => currPlanet[column] < Number(value));
     }
     if (comparison === 'equal') {
       result = results.filter((currPlanet) => currPlanet[column] === Number(value));
@@ -62,11 +63,11 @@ function Filters() {
           data-testid="column-filter"
           onChange={ numericFilters }
         >
-          <option value="population">Population</option>
-          <option value="orbital_period">Orbital period</option>
-          <option value="diameter">Diameter</option>
-          <option value="rotation_period">Rotation period</option>
-          <option value="surface_water">Surface water</option>
+          <option value="population">population</option>
+          <option value="orbital_period">orbital_period</option>
+          <option value="diameter">diameter</option>
+          <option value="rotation_period">rotation_period</option>
+          <option value="surface_water">surface_water</option>
         </select>
         <select
           name="comparison"
