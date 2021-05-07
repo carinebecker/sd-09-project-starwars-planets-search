b97ba83224f37ec95f36a7b20fada0ef6df1c014
Rafael Geronimo <rafaelgeronimo@outlook.com>
2021-05-07 17:13:54 -0300

    Requisito 3

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,7 +1,9 @@
 import React, { useContext } from 'react';
 import context from '../context/contextApi';
 
-function renderTableCollumns(data) {
+import './table.css';
+
+function renderTablecolumns(data) {
   if (data.length === 0) {
     return;
   }
@@ -25,14 +27,39 @@ function renderTableRows(data) {
   );
 }
 
-function Table() {
-  const { data, filters: { filterByName: { name } } } = useContext(context);
+function filterDataByName(data, name) {
   const newData = [...data].filter((planet) => planet.name.includes(name));
   data.forEach((planet) => delete planet.residents);
+  return newData;
+}
+
+function filterDataByValues(data, numericValues) {
+  if (numericValues.length === 0) { return data; }
+  const { comparison, column: keyToCompare, value } = numericValues[0];
+  switch (comparison) {
+  case 'maior que':
+    return [...data].filter((planet) => Number(planet[keyToCompare]) > Number(value));
+  case 'menor que':
+    return [...data].filter((planet) => Number(planet[keyToCompare]) < Number(value));
+  case 'igual a':
+    return [...data].filter((planet) => Number(planet[keyToCompare]) === Number(value));
+  default:
+    return data;
+  }
+}
+
+function Table() {
+  const {
+    data,
+    filters: {
+      filterByName: { name },
+      filterByNumericValues } } = useContext(context);
+  let newData = filterDataByName(data, name);
+  newData = filterDataByValues(newData, filterByNumericValues);
   return (
     <table>
       <thead>
-        {renderTableCollumns(newData)}
+        {renderTablecolumns(newData)}
         {renderTableRows(newData)}
       </thead>
     </table>
--- src/context/Provider.js
+++ src/context/Provider.js
@@ -5,15 +5,19 @@ import context from './contextApi';
 function Provider({ children }) {
   const [planets, setPlanets] = useState([]);
   const [name, setName] = useState('');
+  const [numericValues, setnumericValues] = useState([]);
+
   const contextValue = {
     data: planets,
     filters: {
       filterByName: {
         name,
       },
+      filterByNumericValues: numericValues,
     },
     setPlanets,
     setName,
+    setnumericValues,
   };
   return (
     <context.Provider value={ contextValue }>
