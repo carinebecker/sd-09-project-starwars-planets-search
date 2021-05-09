7dbc7ff1748212be7d97b380243d60dea9aa0e63
Carine Becker <caarine.m@gmail.com>
2021-05-08 22:13:02 -0300

    fix: req6
    Signed-off-by: Carine Becker <caarine.m@gmail.com>

--- src/store.js
+++ src/store.js
@@ -1,60 +1,14 @@
 export default function handleOrderFilter(planets, { column, sort }) {
   const dataToSort = [...planets];
-  const negative = -1;
-  let result;
-  if (sort === 'ASC') {
-    result = dataToSort.sort((a, b) => {
-      if (a[column] > b[column]) {
-        return 1;
-      }
-      if (a[column] < b[column]) {
-        return negative;
-      }
-      return 0;
-    });
-  }
-
-  if (sort === 'DESC') {
-    result = dataToSort.sort((a, b) => {
-      if (a[column] > b[column]) {
-        return negative;
-      }
-      if (a[column] < b[column]) {
-        return 1;
-      }
-      return 0;
-    });
-  }
-  return result;
+  return dataToSort.sort((a, b) => {
+    if (!Number.isNaN(Number(a[column]))) {
+      return sort === 'ASC'
+        ? Number(a[column]) - Number(b[column])
+        : Number(b[column]) - Number(a[column]);
+    }
+    if (a[column] === undefined) return 0;
+    return sort === 'ASC'
+      ? a[column].localeCompare(b[column])
+      : b[column].localeCompare(a[column]);
+  });
 }
-
-// export default function handleOrderFilter() {
-//   const { column, sort } = order;
-//   const dataToSort = [...filteredData];
-//   const negative = -1;
-//   let result;
-//   if (sort === 'ASC') {
-//     result = dataToSort.sort((a, b) => {
-//       if (a[column] > b[column]) {
-//         return 1;
-//       }
-//       if (a[column] < b[column]) {
-//         return negative;
-//       }
-//       return 0;
-//     });
-//   }
-
-//   if (sort === 'DESC') {
-//     result = dataToSort.sort((a, b) => {
-//       if (a[column] > b[column]) {
-//         return negative;
-//       }
-//       if (a[column] < b[column]) {
-//         return 1;
-//       }
-//       return 0;
-//     });
-//   }
-//   setFilteredData(result);
-// }
