57ef6a4095bf0c66c587f36f71e0288c5f235522
Rodrigo Farias <deltadigo@gmail.com>
2021-05-09 00:10:23 -0300

    Requisito 1 - Correções ESLint

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-import Home from '../pages/Home/index';
+// import Home from '../pages/Home/index';
 import Planets from '../pages/Planets/index';
 
 function Routes() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" exact component={ Home } />
-        <Route path="/planets" exact component={ Planets } />
+        <Route path="/" exact component={ Planets } />
+        {/* <Route path="/planets" exact component={ Planets } /> */}
       </Switch>
     </BrowserRouter>
   );
