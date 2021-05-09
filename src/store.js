a47716be9112cb6ce906d52a66cd414b3b830836
Carine Becker <caarine.m@gmail.com>
2021-05-08 22:13:34 -0300

    fix: lint
    Signed-off-by: Carine Becker <caarine.m@gmail.com>

--- src/store.js
+++ src/store.js
@@ -1,12 +1,12 @@
 export default function handleOrderFilter(planets, { column, sort }) {
   const dataToSort = [...planets];
   return dataToSort.sort((a, b) => {
+    if (a[column] === undefined) return 0;
     if (!Number.isNaN(Number(a[column]))) {
       return sort === 'ASC'
         ? Number(a[column]) - Number(b[column])
         : Number(b[column]) - Number(a[column]);
     }
-    if (a[column] === undefined) return 0;
     return sort === 'ASC'
       ? a[column].localeCompare(b[column])
       : b[column].localeCompare(a[column]);
