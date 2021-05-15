ecdc2207a0d9c236ac96bdcef39258d2456d4e91
negres <nataliaknegres@gmail.com>
2021-05-15 11:22:06 -0300

    Adiciona requisito 1 / corrige Table

---
+++ src/components/Table.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import StarWarsContext from '../context/StarWarsContext';
+
+function Table() {
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>name</th>
+            <th>rotation_period</th>
+            <th>orbital_period</th>
+            <th>diameter</th>
+            <th>climate</th>
+            <th>gravity</th>
+            <th>terrain</th>
+            <th>surface_water</th>
+            <th>population</th>
+            <th>films</th>
+            <th>created</th>
+            <th>edited</th>
+            <th>url</th>
+          </tr>
+        </thead>
+        <tbody>
+          <StarWarsContext.Consumer>
+            {(value) => {
+              if (value) {
+                console.log(value);
+                return value.map((planet) => (
+                  <tr key={ planet.name }>
+                    {Object.keys(planet)
+                      .filter((key) => key !== 'residents')
+                      .map((key) => <td key={ key }>{planet[key]}</td>)}
+                  </tr>
+                ));
+              }
+            }}
+          </StarWarsContext.Consumer>
+        </tbody>
+      </table>
+      <h1>Table</h1>
+    </div>
+  );
+}
+
+export default Table;
