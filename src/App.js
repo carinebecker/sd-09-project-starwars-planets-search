6b3c2716f3c30104ef1a2751a3b47c1cffb0ad64
negres <nataliaknegres@gmail.com>
2021-05-15 10:37:08 -0300

    Cria requisição para API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import StarWarsContext from './context/StarWarsContext';
 import './App.css';
 
 function App() {
+  const [responseApi, setResponseApi] = useState();
+
+  useEffect(() => {
+    if (responseApi === undefined) {
+      const fetchApi = async () => {
+        try {
+          const data = await fetch('https://swapi-trybe.herokuapp.com/api/planets/')
+            .then((response) => response.json());
+          setResponseApi(data.results);
+          console.log(data.results);
+        } catch (error) {
+          console.log(error);
+        }
+      };
+      fetchApi();
+    }
+  }, [responseApi]);
+
   return (
     <span>Hello, App!</span>
   );
