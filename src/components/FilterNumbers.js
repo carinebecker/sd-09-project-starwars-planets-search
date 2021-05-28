7c2aaa0fadfce02e1a6471326583b32bcc095c71
Bruno <brunocmoraes97@gmail.com>
2021-05-28 19:11:00 -0300

    Correçao lint

--- src/components/FilterNumbers.js
+++
@@ -1,9 +0,0 @@
-const initialState = filterByName.filter((itens) => {
-  if (activateButton === true) {
-    console.log(value);
-    if (comparison === 'maior que') return itens[column] > value;
-    if (comparison === 'igual a') return itens[column] === value;
-    if (comparison === 'menor que') return itens[column] < value;
-  }
-  return data;
-});
