2976f7a3173e92296cb5935a207ac1aa1c529eb5
João Pedro Barros <joaopedrobarros5@gmail.com>
2021-05-30 09:54:39 -0300

    capturando dados

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -1,15 +1,27 @@
 import React, { createContext, useState } from 'react';
+import planetsDataAPI from '../services/StarWarsAPI';
 
 export const Context = createContext({});
 
 export function ContextProvider({ children }) {
   const [data, setData] = useState([]);
 
-  return(
-    <ContextProvider value={{
-      data, setData
-    }}>
+  const getData = async () => {
+    const result = await planetsDataAPI();
+    setData(result);
+  };
+
+  return (
+    <ContextProvider
+      value={ {
+        data,
+        setData,
+        getData,
+      } }
+    >
       { children }
     </ContextProvider>
   );
 }
+
+ContextProvider.propTypes = { children: React.ReactNode }.isRequired;
