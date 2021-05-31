22d6ece538ff864c0cc98da223e009ad6cde51ee
Uibirá <usbarreto@gmail.com>
2021-05-30 23:38:30 -0300

    Insere requisitos 1, 2 e 3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import React from 'react';
+import Table from './components/Table';
+import Header from './components/Header';
+import Provider from './context/Provider';
 import './App.css';
 
 function App() {
   return (
-    <span>A long time ago in a galaxy far far away...</span>
+    <Provider>
+      <span>A long time ago in a galaxy far far away...</span>
+      <Header />
+      <Table />
+    </Provider>
   );
 }
 
 export default App;
+
+// import React from 'react';
+// import Provider from './context/Provider';
+// import './App.css';
+// import Table from './Components/Table';
+// import Header from './Components/Header';
+
+// function App() {
+//   return (
+//     <Provider>
+//       <h1>StarWars</h1>
+//       <Header />
+//       <Table />
+//     </Provider>
+//   );
+// }
+
+// export default App;
--- src/components/SourceAPI.js
+++ src/components/SourceAPI.js
@@ -1,11 +1,20 @@
-const AND_POINT_API = 'https://swapi-trybe.herokuapp.com/api/planets/';
-const getDataPlanetsStarWars = () => (
-  fetch(AND_POINT_API)
-    .then((response) => (
-      response
-        .json()
-        .then((json) => (response.ok ? Promise.resolve(json) : Promise.reject(json)))
-    ))
-);
+// const AND_POINT_API = 'https://swapi-trybe.herokuapp.com/api/planets/';
+// const getPlanetsData = () => (
+//   fetch(AND_POINT_API)
+//     .then((response) => (
+//       response
+//         .json()
+//         .then((json) => (response.ok ? Promise.resolve(json) : Promise.reject(json)))
+//     ))
+// );
 
-export default getDataPlanetsStarWars;
+// export default getPlanetsData;
+
+const url = 'https://swapi-trybe.herokuapp.com/api/planets/';
+
+const planetsAPI = async () => {
+  const results = await fetch(url).then((response) => response.json());
+  return results;
+};
+
+export default planetsAPI;
