eaf87de6fcf388f453d26d63c69724efb8689fe2
Uibirá <usbarreto@gmail.com>
2021-05-31 11:46:47 -0300

    Fatality: bye bye front-end...

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import Table from './components/Table';
 import Header from './components/Header';
 import Provider from './context/Provider';
 import './App.css';
+import StarWars from './StarWars.svg';
 
 function App() {
   return (
     <Provider>
+      <img src={ StarWars } alt=" " width="270px" />
+      <p />
       <span>A long time ago in a galaxy far far away...</span>
       <Header />
       <Table />
